4317a48a90b5f235c9c4a476c1a66a5849409da5
earGO <rh.edwin@gmail.com>
2019-02-08 17:55:17 +0300

    working on google-oauth

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+
+/*======================Declare all libraries and middleware and stuff ========================*/
 const express = require('express'),
     mongoose = require('mongoose'),
     app = express(),
-    port = process.env.PORT || 5000;
+    port = process.env.PORT || 5000,
 
+        //routers
+    auth = require('./routers/auth'),
+
+        //middleware imports
+    passport = require('passport');
+
+/*====================== Mongoose connections ========================*/
+
+/*====================== Activate middleware ========================*/
+
+/*====================== Global variables ========================*/
+
+//Passport Config
+require('./config/passport')(passport)
+
+/*====================== Go routing!!!! ========================*/
 app.get('/',(req,res)=>{
     res.send('Server up and running for Storybook')
 })
 
+app.use('/auth',auth)
+
+app.get('/dashboard',(req,res)=>{
+    res.send('dashboard gonna be here')
+})
+
+/*====================== Start an application ========================*/
 app.listen(port,()=>{
     console.log(`Storybook App is up on port ${port}`)
 })
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:"mongodb://earGO:ST)RYB))K-d3v@ds227185.mlab.com:27185/storybooks-dev",
+    googleClientID:"715083241931-632pm5klqqehj5mn6gmqc0gefelpki3r.apps.googleusercontent.com",
+    googleClientSecret:"IJvmVBVegjExtdZ_e_Z-BunJ"
+}
